5ce16af42136026d504d65204db7e1e66d482a7c
yuna <yuna@rarajob.com>
2025-02-10 12:10:42 +0900

    :recycle: 노란줄 리팩토링

--- src/level0/Solved_06.java
+++ src/level0/Solved_06.java
@@ -2,7 +2,6 @@ package level0;
 
 public class Solved_06 {
     public int solution(int num1, int num2) {
-        int answer = num1 * num2;
-        return answer;
+        return num1 * num2;
     }
 }
--- src/level0/Solved_21.java
+++ src/level0/Solved_21.java
@@ -4,13 +4,13 @@ import java.util.Objects;
 
 public class Solved_21 {
     public String solution(String my_string, String letter) {
-        String answer = "";
+        StringBuilder answer = new StringBuilder();
         String[] stringArr = my_string.split("");
         for (String s : stringArr) {
             if (!Objects.equals(s, letter)) {
-                answer += s;
+                answer.append(s);
             }
         }
-        return answer;
+        return answer.toString();
     }
 }
